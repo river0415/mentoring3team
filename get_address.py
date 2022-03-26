55a3d576b466556be36c6940cce70e910237e2f3
Junyong <proice22@gmail.com>
2022-03-26 11:45:42 +0900

    3/26 add api map

--- get_address.py
+++ get_address.py
@@ -4,12 +4,13 @@ r=open('동물병원_utf8.csv', encoding='utf-8')
 data=csv.reader(r)
 header=next(data)
 address=[]
+num=[]
 for rr in data :
     address.append(rr[2])
+    num.append(int(rr[0]))
 for i in range(len(address)):
     a = address[i].split(' ')
     address[i] = " ".join(a[0:4])
-r.close()
 
 ####### 도로명주소 위도 경도 값으로 바꿔주기 ########
 from geopy.geocoders import Nominatim
@@ -27,3 +28,15 @@ longitude =[]
 for i in address:
     latitude.append(geocoding(i)[0])
     longitude.append(geocoding(i)[1])
+
+conn = sqlite3.connect('C:/Users/river/PycharmProjects/mentoring3team/mentoring3team.db', isolation_level=None)
+
+c = conn.cursor()
+
+sqlite_update_query = """Update hospital set latitude = ?, longitude = ? where num = ?"""
+for i in range(len(address)):
+    columnValue = (latitude[i], longitude[i], num[i])
+    c.execute(sqlite_update_query, columnValue)
+
+conn.close()
+r.close()
