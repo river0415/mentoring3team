635e1b259f7cefe5cb58a8ad2f15c1c788b0e1ca
mason <cortm5pp@gmail.com>
2022-04-02 16:49:21 +0900

    지도 위치 수정

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 # from flask_sqlalchemy import SQLAlchemy
 from models import db, migrate
 import routes.hosp_route as hr
+import routes.mem_route as mr
 
 import config
 
@@ -10,9 +11,13 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 app.register_blueprint(hr.bp)
+app.register_blueprint(mr.bp)
+
 
 migrate.init_app(app, db)
 
+
+db.init_app(app)
 @app.route('/')
 def root():
     return render_template('index.html')
--- routes/mem_route.py
+++ routes/mem_route.py
@@ -1,1 +1,8 @@
 from flask import render_template, request, Blueprint, session
+
+bp = Blueprint('member', __name__, url_prefix='/mem')
+
+
+@bp.route('/login', methods=['GET'])
+def login():
+    return render_template('member/index2.html')
